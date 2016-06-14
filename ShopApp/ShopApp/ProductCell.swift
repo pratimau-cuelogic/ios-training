e670f4c2fadb324e2a85b1b528686308228cdfd0
Pratima Umbardand <pratimau@pratimas-mac-mini.local>
2016-06-14 18:41:21 +0530

    Committing code for Product and Cart view controllers.

---
+++ ShopApp/ShopApp/ProductCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProductCell.swift
+//  ShopApp
+//
+//  Created by Pratima Umbardand on 14/06/16.
+//  Copyright © 2016 Pratima Umbardand. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
